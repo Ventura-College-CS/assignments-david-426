14c320baa99c35a5c9799bdeb0a15a9d2767862a
david-426 <davidperea426@gmail.com>
2021-11-29 21:22:03 -0800

    changed values on line 1

--- Midterm/q3.cpp
+++ Midterm/q3.cpp
@@ -4,5 +4,14 @@ using namespace std;
 
 int main()
 {
-
+    int numbers[] = {10, 20, 15, 30, 40, 50}; //changed the values on line 1
+	deque<int> dq(numbers, numbers + 3);
+	cout << " Dequeued : " << dq.front() << endl;
+	dq.pop_front();
+	cout << " Dequeued : " << dq.front() << endl;
+	dq.pop_front();
+	cout << " Dequeued from rear: " << dq.back() << endl;
+	dq.pop_back();
+	cout << " Dequeued from rear: " << dq.back() << endl;
+	dq.pop_back();
 }
