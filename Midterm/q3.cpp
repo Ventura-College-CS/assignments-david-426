904bfb4163e456aa3bbe2895da6052de74e12e6b
david-426 <davidperea426@gmail.com>
2021-11-29 21:25:41 -0800

    showed declaration change

--- Midterm/q3.cpp
+++ Midterm/q3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int numbers[] = {10, 20, 15, 30, 40, 50}; //changed the values on line 1
-	deque<int> dq(numbers, numbers + 3);
+	deque<int> dq(numbers + 1, numbers + 4); // showing a variation of the declaration on line 2
 	cout << " Dequeued : " << dq.front() << endl;
 	dq.pop_front();
 	cout << " Dequeued : " << dq.front() << endl;
