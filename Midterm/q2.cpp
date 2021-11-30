35561c28ce00fe40b7140f603d28ebb1425beaf3
david-426 <davidperea426@gmail.com>
2021-11-29 20:42:04 -0800

    check

--- Midterm/q2.cpp
+++ Midterm/q2.cpp
@@ -7,11 +7,21 @@ class MyCompareFunction
     public:
         int operator()(const int &lhs, const int &rhs)
         {
-            int l, r;
+            return lhs < rhs; // this returns the larger number
         }
 };
 
 int main()
 {
+	priority_queue<int, vector<int>, MyCompareFunction> pq;
 
+	pq.push(31);
+	pq.push(45);
+	pq.push(29);
+
+	while (!pq.empty())
+	{
+		cout << "Poped Value : " << pq.top() << endl;
+		pq.pop();
+	}
 }
