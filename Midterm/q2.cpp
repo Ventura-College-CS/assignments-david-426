560e3ef8ab6249701e4928854c621d41eaf1c1ae
david-426 <davidperea426@gmail.com>
2021-11-29 20:31:44 -0800

    check

---
+++ Midterm/q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class MyCompareFunction
+{
+    public:
+        int operator()(const int &lhs, const int &rhs)
+        {
+            int l, r;
+        }
+};
+
+int main()
+{
+
+}
