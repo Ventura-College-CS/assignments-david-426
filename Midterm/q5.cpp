992a7d1a2ff08773bffa9bd4332aa9337081036c
david-426 <davidperea426@gmail.com>
2021-11-29 22:19:55 -0800

    check

--- Midterm/q5.cpp
+++ Midterm/q5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Student
@@ -8,12 +9,62 @@ class Student
         string sname;
         double scores[3];
     public:
-        Student();
-        Student(int i, string name, double *sc);
-        int getID();
-        string getName();
-        double * getScores();
-        void setID();
-        void setName();
-        void setScores();
+        Student(){}
+        Student(int i, string name, double *sc)
+        {
+
+        }
+        int getID()
+        {
+
+        }
+        string getName()
+        {
+
+        }
+        double * getScores()
+        {
+
+        }
+        void setID()
+        {
+
+        }
+        void setName()
+        {
+
+        }
+        void setScores()
+        {
+
+        }
+};
+
+template <class T, int capacity = 30>
+class Stack
+{
+    private:
+        vector<T> pool;
+    public:
+        Stack(){}
+        void push(const T &el)
+        {
+
+        }
+        t pop()
+        {
+
+        }
+        T topEl()
+        {
+
+        }
+        bool isEmpty()
+        {
+
+        }
+        int size()
+        {
+
+        }
 };
