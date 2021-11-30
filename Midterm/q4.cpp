944ec60f5c740e35889cbbb8774a38e1e77c8f26
david-426 <davidperea426@gmail.com>
2021-11-29 22:02:06 -0800

    check

--- Midterm/q4.cpp
+++ Midterm/q4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    list <int> list1(5, 100);
+    list <int> list1(5, 100); // changing values changes how many of which value is stored
     queue<int, list<int>> lq(list1);
     cout << "Dequeued : " << lq.front() << endl;
     lq.pop();
