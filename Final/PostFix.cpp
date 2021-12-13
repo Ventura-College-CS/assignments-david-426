a96a076cfcb2b68158f4d2ce60d6cd43cb2bdda2
david-426 <davidperea426@gmail.com>
2021-12-13 08:44:00 -0800

    check

--- Final/PostFix.cpp
+++ Final/PostFix.cpp
@@ -56,4 +56,27 @@ void PostFixExp::printExpression() const
     }
     cout << endl;
 };
+int PostFixExp::Evaluation()
+{
+    const int N = 20; //maximum size of the stack
+    const int offset = 48; // in order to get the correct digits when extracting them from the expression string the given offset needs to be taken into account
+    Stack<int, N> stack; //creating the stack using a vector
+    char input;
 
+    int n1, n2, result;
+
+    for (int i = 0; i < expression.size(); i++)
+    {
+        input = expression[i]; // taking a single character from the string
+        if (isdigit(input)) // checking if the character is a digit using an inbuilt c++ function
+            stack.push(int(input) - offset); // if it is a digit the char is converted to an int and the offset is subtracted to push the true value into the stack
+        else if (isOperator(input))//checking if the non digit is an operator or not
+        {
+            n1 = stack.pop(); //taking the most recent digit and storing it into an integer variable
+            n2 = stack.pop(); // taking the second most recent digit and storing it
+            result = calculate(n1, n2, input); // calculating the operation given based on the operator and two topmost digits
+            stack.push(result); // storing the result in the vector so that it is operated on with the next most recent digit
+        }
+    }
+    return stack.pop(); // returning the fully calculated result
+};
--- Final/final3.cpp
+++ Final/final3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 #include "PostFix.h"
 using namespace std;
 
-
 int main()
 {
+    string exp;
     cout << "Please Enter your postfix expression";
 }
