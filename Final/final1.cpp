c9154c2d86363ccb1e653c55bf19d57ab1190ff5
david-426 <davidperea426@gmail.com>
2021-12-13 06:04:22 -0800

    check

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Course
+{
+    private:
+        int cID; // course ID
+        string cName; // course name
+        int cCredit; // course credit
+    public:
+        Course(): cID(0), cName(){}
+        Course(int id, string name, int credit)
+        {
+            cID = id;
+            cName = name;
+            cCredit = credit;
+        }
+        int getID()
+        {
+            return cID;
+        }
+        int getCredit()
+        {
+            return cCredit;
+        }
+        string getName()
+        {
+            return cName;
+        }
+};
 
 int main()
 {
