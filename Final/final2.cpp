671a0d9dbc64b3e775ee3757d4a6162be1a8f461
david-426 <davidperea426@gmail.com>
2021-12-13 07:10:30 -0800

    q2 started

---
+++ Final/final2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string> // in order to have user input for string
+using namespace std;
+
+class Course
+{
+    private:
+        int cID; // course ID
+        string cName; // course name
+        int cCredit; // course credit
+    public:
+        Course(): cID(0), cName(){}
+        Course(int id, string name, int credit)
+        {
+            cID = id;
+            cName = name;
+            cCredit = credit;
+        }
+        int getID()
+        {
+            return cID;
+        }
+        int getCredit()
+        {
+            return cCredit;
+        }
+        string getName()
+        {
+            return cName;
+        }
+        void printCourse()
+        {
+            cout << "Course ID: " << cID << "  Course Name: " << cName << "  Course units:" << cCredit << endl; 
+        }
+};
