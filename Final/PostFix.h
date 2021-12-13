055dffece881e364325c8e5481c1a1898371dbe0
david-426 <davidperea426@gmail.com>
2021-12-13 08:32:40 -0800

    made the postfix header file

---
+++ Final/PostFix.h
@@ -0,0 +1,22 @@
+#ifndef POSTFIX_H
+#define POSTFIX_H
+
+#include "stack.h" // general stack file given to us in class
+#include <string>
+using namespace std;
+
+class PostFixExp
+{
+    private: // creating the PostFixExp private variables
+        string expression;
+        int isOperator(char);
+        int calculate(int, int, char); 
+    public:
+        PostFixExp();
+        PostFixExp(string);
+        void setExpression(string);
+        void printExpression() const;
+        int Evaluation();
+};
+
+#endif
